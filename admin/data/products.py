ccfca70dde87d6789a169374eba604b29039abbe
dimondg <dimon89033439971@mail.ru>
2022-03-27 16:16:34 +0300

    suoer_comit

---
+++ admin/data/products.py
@@ -0,0 +1,15 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
+
+
+class Product(SqlAlchemyBase):
+    __tablename__ = 'products'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    image_file_path = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
---
+++ admin/main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from data import db_session
+from data.products import Product
+import csv
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'baker_admin_secret_key'
+
+with open('csv_files/products.csv', encoding="utf8") as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+    products = list(reader)
+
+
+db_session.global_init("db/assortment.db")
+
+db_sess = db_session.create_session()
+product = db_sess.query(Product).first()
+
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    return render_template('no.html', title=product.title,
+                           about=product.about, img_path=product.image_file_path)
+
+
+def main():
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
