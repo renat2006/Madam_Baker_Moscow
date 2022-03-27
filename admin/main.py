8ba402c93a4631f3763af3d06d11373f0417afd1
Renat Gubaydullin <renatgubaudullin@gmail.com>
2022-03-27 16:33:47 +0300

    5.0

--- admin/main.py
+++ admin/main.py
@@ -3,7 +3,7 @@ from data import db_session
 from data.products import Product
 import csv
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=".")
 app.config['SECRET_KEY'] = 'baker_admin_secret_key'
 
 with open('csv_files/products.csv', encoding="utf8") as csvfile:
@@ -19,7 +19,7 @@ product = db_sess.query(Product).first()
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
-    return render_template('no.html', title=product.title,
+    return render_template('index.html', title=product.title,
                            about=product.about, img_path=product.image_file_path)
 
 
--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+from flask import Flask, render_template
+from data import db_session
+from data.products import Product
+import csv
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__, template_folder=".")
+app.config['SECRET_KEY'] = 'baker_admin_secret_key'
+
+with open('csv_files/products.csv', encoding="utf8") as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+    products = list(reader)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+db_session.global_init("db/assortment.db")
+
+db_sess = db_session.create_session()
+product = db_sess.query(Product).first()
 
 
-# Press the green button in the gutter to run the script.
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    return render_template('no.html', title=product.title,
+                           about=product.about, img_path=product.image_file_path)
+
+
+def main():
+    app.run()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
