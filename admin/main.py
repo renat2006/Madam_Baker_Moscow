086cd5e6c077e05f8bb74101e7b4d9772922164e
dimondg <dimon89033439971@mail.ru>
2022-03-28 15:26:06 +0300

    add append assortment item and delete func

--- admin/main.py
+++ admin/main.py
@@ -5,6 +5,7 @@ from data import db_session
 from data.products import Product
 from werkzeug.utils import secure_filename
 from edit_form import EditForm
+from add_form import AddForm
 
 UPLOAD_FOLDER = '/static/img/product'
 
@@ -49,6 +50,38 @@ def edit(index):
             db_sess.commit()
             return redirect("/admin")
         return redirect("/")
+
+
+@app.route('/delete/<index>', methods=['GET', 'POST'])
+def delete(index):
+    db_sess = db_session.create_session()
+    item = db_sess.query(Product).get(int(index))
+    print(product[int(index) - 1].title)
+    db_sess.delete(item)
+    db_sess.commit()
+    return redirect("/admin")
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    form = AddForm()
+    if request.method == 'GET':
+        return render_template('add.html', product=product, form=form)
+    elif request.method == 'POST':
+        if form.validate_on_submit():
+            f = request.files['file']
+            filename = secure_filename(f.filename)
+            f.save(f'static/img/product/{filename}')
+            db_sess = db_session.create_session()
+            item = Product()
+            item.title = form.title.data
+            item.about = form.content.data
+            item.image_file_path = filename
+            db_sess.add(item)
+            db_sess.commit()
+            return redirect("/admin")
+        return redirect("/")
+
 
 
 def main():
