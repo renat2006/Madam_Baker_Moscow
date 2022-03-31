ed3c2263a96e80029305b7d79075f5acb5fe0249
dimondg <dimon89033439971@mail.ru>
2022-03-31 17:11:37 +0300

    ideal

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
-from flask import Flask, render_template
-from data import db_session
-from data.products import Product
+from flask import Flask, render_template, url_for
+from admin.data import db_session
+from admin.data.products import Product
 import csv
 
 app = Flask(__name__, template_folder=".")
 app.config['SECRET_KEY'] = 'baker_admin_secret_key'
 
-with open('csv_files/products.csv', encoding="utf8") as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
-    products = list(reader)
+
+db_session.global_init("admin/db/assortment.db")
 
 
-db_session.global_init("db/assortment.db")
-
-db_sess = db_session.create_session()
-product = db_sess.query(Product).first()
-
-
-@app.route('/admin', methods=['GET', 'POST'])
-def admin():
-    return render_template('no.html', title=product.title,
-                           about=product.about, img_path=product.image_file_path)
+@app.route('/madam_baker', methods=['GET', 'POST'])
+def baker():
+    db_sess = db_session.create_session()
+    products = db_sess.query(Product).all()
+    print(products[0].title)
+    return render_template('index.html', products=products)
 
 
 def main():
